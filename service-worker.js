875f3f5e58e680564597df1154802038c1a53777
zakoszak747-source <zakoszak747@gmail.com>
2026-09-23 08:59:09 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 // (ex. Netlify) que dans un sous-dossier (ex. GitHub Pages
 // username.github.io/nom-du-repo/).
 
-const CACHE_NAME = "reunion-ia-cache-v25";
+const CACHE_NAME = "reunion-ia-cache-v26";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
